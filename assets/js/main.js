4cae9300178f1fb7366b02646f377b9542f44ccf
JeanPCB <jeanp.cebinel01@gmail.com>
2022-05-23 02:29:30 -0300

    Adição: checagem de condições

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,25 @@
 const markPlaces = document.querySelectorAll( '.markPlace' );
 let counter = 0;
-let playsX = [];
-let playsO = []
+let playerX = [];
+let playerO = [];
+const possibleEnds = [
+    [ '1', '2', '3' ],
+    [ '1', '4', '7' ],
+    [ '1', '5', '9' ],
+    [ '2', '5', '8' ],
+    [ '3', '6', '9' ],
+    [ '3', '5', '7' ],
+    [ '4', '5', '6' ],
+    [ '7', '8', '9' ]
+];
 
 for( let markPlace of markPlaces) {
     markPlace.addEventListener( 'click', setPlays );
+    markPlace.addEventListener( 'click', () => {
+        if( playerTurn().length >= 3) {
+            console.log( checkEnds( playerTurn() ) );
+        }
+    });
     markPlace.addEventListener( 'click', setActionsDisplay );
 }
 
@@ -21,6 +36,29 @@ function setActionsDisplay( e ) {
 }
 
 function setPlays( e ) {
-    counter == 0 ? playsX.push( e.target.id ) : playsO.push( e.target.id );
+    counter == 0 ? playerX.push( e.target.id ) : playerO.push( e.target.id );
 }
 
+function playerTurn() {
+    if( counter == 0 )
+        return playerX;
+    else
+        return playerO;
+}
+
+function checkEnds( plays ) {
+    for( let index in possibleEnds ){
+      var checkValue = endGameCheck( plays, index );
+      if( checkValue )
+          break;
+    }
+    return checkValue;
+}
+
+function endGameCheck( plays, index ) {
+    if( plays.length >= 3 ) {
+        return possibleEnds[index].every( value => {
+            return plays.includes( value );
+        } );
+    }
+}
