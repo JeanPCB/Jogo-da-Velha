97b07c5b7b22fb3d640d155c24b42b9087603359
Jean P. Cebinel <92955454+JeanPCB@users.noreply.github.com>
2022-05-24 19:53:21 -0300

    Finalização => vers~

---
+++ assets/js/gameOverEvents.js
@@ -0,0 +1,48 @@
+const playAgainDiv = document.querySelector('.playAgainDiv');
+const playAgainBtn = document.querySelector('.playAgainBtn');
+const body = document.querySelector('body');
+
+function gameOverScreen() {
+    gameOverScreenHeight();
+    gameRestart();
+    if (isGameOver() == true) {
+        divGameOver.style.transform = 'translateY(0)';
+    }
+}
+
+function gameRestart() {
+    playAgainDiv.addEventListener('click', () => {
+        playAgainBtn.style.right = '0';
+        playAgainDiv.style.backgroundColor = '#000';
+        setTimeout(() => divGameOver.style.transform = 'translateY(-200%)', 1000);
+        clearPlays();
+        setTimeout(clearBoard, 1000);
+        setTimeout(resetActionsDisplay, 1000);
+        setTimeout(resetRestartBtnAnim, 2000);
+    });
+}
+
+function resetRestartBtnAnim() {
+    playAgainBtn.style.right = '100%';
+}
+
+function clearBoard() {
+    for (let markPlace of markPlaces) {
+        markPlace.innerHTML = '';
+    }
+}
+
+function gameOverScreenHeight() {
+    divGameOver.style.height = `${body.offsetHeight}px`;
+}
+
+function resetActionsDisplay() {
+    for (let markPlace of markPlaces) {
+        markPlace.style = 'pointer-events = default';
+    }
+}
+
+function clearPlays() {
+    playerX = [];
+    playerO = [];
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,15 @@
-const markPlaces = document.querySelectorAll( '.markPlace' );
-const divGameOver = document.querySelector( '#gameOver' );
+const markPlaces = document.querySelectorAll('.markPlace');
+const divGameOver = document.querySelector('#gameOver');
 
 const possibleEnds = [
-    [ '1', '2', '3' ],
-    [ '1', '4', '7' ],
-    [ '1', '5', '9' ],
-    [ '2', '5', '8' ],
-    [ '3', '6', '9' ],
-    [ '3', '5', '7' ],
-    [ '4', '5', '6' ],
-    [ '7', '8', '9' ]
+    ['1', '2', '3'],
+    ['1', '4', '7'],
+    ['1', '5', '9'],
+    ['2', '5', '8'],
+    ['3', '6', '9'],
+    ['3', '5', '7'],
+    ['4', '5', '6'],
+    ['7', '8', '9']
 ];
 
 let counter = 0;
@@ -17,15 +17,15 @@ let playerX = [];
 let playerO = [];
 
 
-for( let markPlace of markPlaces) {
-    markPlace.addEventListener( 'click', setPlays );
-    markPlace.addEventListener( 'click', setActionsDisplay );
-    markPlace.addEventListener( 'click', isGameOver );
-    markPlace.addEventListener( 'click', gameOverScreen );
+for (let markPlace of markPlaces) {
+    markPlace.addEventListener('click', setPlays);
+    markPlace.addEventListener('click', setActionsDisplay);
+    markPlace.addEventListener('click', isGameOver);
+    markPlace.addEventListener('click', gameOverScreen);
 }
 
-function setActionsDisplay( e ) {
-    if( counter == 0 ) {
+function setActionsDisplay(e) {
+    if (counter == 0) {
         e.target.innerHTML = '<img class="markPlaceDisabled" src="./assets/images/crossPlayer.png">';
         e.target.style = 'pointer-events: none';
         counter = 1;
@@ -36,52 +36,38 @@ function setActionsDisplay( e ) {
     }
 }
 
-function setPlays( e ) {
-    counter == 0 ? playerX.push( e.target.id ) : playerO.push( e.target.id );
+function setPlays(e) {
+    counter == 0 ? playerX.push(e.target.id) : playerO.push(e.target.id);
 }
 
 function lastPlay() {
-    if( counter == 0 )
+    if (counter == 0)
         return playerO;
     else
         return playerX;
 }
 
-function gameOver( plays ) {
-    for( let index in possibleEnds ){
-      var checkValue = endGameCheck( plays, index );
-      if( checkValue )
-          break;
+function gameOver(plays) {
+    for (let index in possibleEnds) {
+        var checkValue = endGameCheck(plays, index);
+        if (checkValue)
+            break;
     }
     return checkValue;
 }
 
-function endGameCheck( plays, index ) {
-    if( plays.length >= 3 ) {
-        return possibleEnds[index].every( value => {
-            return plays.includes( value );
-        } );
+function endGameCheck(plays, index) {
+    if (plays.length >= 3) {
+        return possibleEnds[index].every(value => {
+            return plays.includes(value);
+        });
     }
 }
 
 function isGameOver() {
-    if( lastPlay().length == 5 ) {
+    if (lastPlay().length == 5) {
         return true;
-    }else if( lastPlay().length >= 3) {
-       return gameOver( lastPlay() );
-    }
-}
-
-function gameOverScreen() {
-    gameOverScreenHeight();
-    gameRestart();
-    if( isGameOver() == true ) {
-        divGameOver.style.transform = 'translateY(0)';
-    }
-}
-
-function clearBoard() {
-    for( let markPlace of markPlaces ) {
-        markPlace.innerHTML = '';
+    } else if (lastPlay().length >= 3) {
+        return gameOver(lastPlay());
     }
 }
