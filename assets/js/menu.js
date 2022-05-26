d8bc8c72695f4c32138efcb2d5432024360cc589
JeanPCB <jeanp.cebinel01@gmail.com>
2022-05-26 20:27:50 -0300

    Ajustes no main menu

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,12 +1,17 @@
 const mainMenu = document.querySelector('#main-menu');
 const menuOptions = document.querySelectorAll('.menu-option-btn');
 const gameBoard = document.querySelector('#game-board');
+const body = document.querySelector('body');
 
 for (let menuOption of menuOptions) {
     menuOption.addEventListener('click', startGame);
 }
 
 function startGame() {
+    setTimeout(() => {
+        body.style.overflow = 'auto';
+        mainMenu.style.display = 'none';
+    }, 3000);
     mainMenu.style.transform = 'translateY(200%)';
     gameBoard.style.opacity = '1';
 }
